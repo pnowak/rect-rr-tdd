969264a0dd2cfb31893f8415dca452e78b345431
Piotr Nowak <piotr.nowak.25.02.1978@gmail.com>
2020-10-08 10:06:01 +0200

    Prepare to build

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Editor } from './features/editor/Editor';
+
+ReactDOM.render(<Editor />, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,21 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.ts",
+  mode: 'development',
+  entry: './src/index.tsx',
   output: {
-    path: path.resolve("dist"),
-    filename: "main.js",
+    path: path.resolve('dist'),
+    filename: 'main.js',
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: ['.ts', '.tsx', '.js'],
   },
   devServer: {
-    contentBase: "./dist",
+    contentBase: './dist',
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   module: {
-    rules: [{ test: /\.tsx?$/, loader: "ts-loader" }],
+    rules: [{ test: /\.tsx?$/, loader: 'ts-loader' }],
   },
 };

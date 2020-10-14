bac24979cf550702569fadb77d7d06a270c721dc
Piotr Nowak <piotr.nowak.25.02.1978@gmail.com>
2020-10-14 21:53:31 +0200

    Gallery initially nothing to show

--- test/Gallery.test.tsx
+++ test/Gallery.test.tsx
@@ -7,15 +7,21 @@ import {
 import { Gallery } from '../src/features/gallery/Gallery';
 
 describe('Gallery', () => {
-  let element: Element, render: Render;
+  let element: Element, render: Render, container: HTMLDivElement;
 
   beforeEach(() => {
-    ({ element, render } = createContainer());
+    ({ element, render, container } = createContainer());
   });
 
   it('renders a div with the right id', () => {
     render(<Gallery />);
 
     expect(element('div#gallery')).not.toBeNull()      
-  })
+  });
+
+  it('initially nothing to show', () => {
+    render(<Gallery />);
+
+    expect(container.textContent).toMatch('');
+  });
 })

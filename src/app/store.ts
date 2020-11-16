63df94abbda148dbe895db371651b4b45d2ee2c3
Piotr Nowak <piotr.nowak.25.02.1978@gmail.com>
2020-11-16 20:30:31 +0100

    configureStoreWithLocalStorage

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,17 @@
 import { applyMiddleware, createStore, compose } from 'redux';
 import { editorReducer } from '../features/editor/reducer';
-import { save } from './localStorage';
+import { save, load } from './localStorage';
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export const configureStore = (storeEnhancers = []) => {
+export const configureStore = (storeEnhancers = [], initialState = {rects: []}) => {
   const store = createStore(
     editorReducer,
-    compose(...[applyMiddleware(save), ...storeEnhancers])
+    initialState,
+    compose(...[applyMiddleware(save), ...storeEnhancers]),
   );
 
   return store;
 };
+
+export const configureStoreWithLocalStorage = () =>
+  configureStore(undefined, load());
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { App } from './app/App';
-import { configureStore } from './app/store';
+import { configureStoreWithLocalStorage } from './app/store';
 
 ReactDOM.render(
-  <Provider store={configureStore()}>
+  <Provider store={configureStoreWithLocalStorage()}>
     <App />
   </Provider>, 
   document.getElementById('root')
